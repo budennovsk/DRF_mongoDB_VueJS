ab4b6993a7e8f2b82499e7ff2f7f36e3312bed93
Zin <svetloewill@mail.ru>
2023-05-12 21:45:28 +0300

    first

---
+++ mongo/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from django.core.files.storage import default_storage
+from .models import *
+from .serializers import *
+
+
+# Create your views here.
+
+@csrf_exempt
+def departmentApi(request, id=0):
+    if request.method == 'GET':
+        departaments = Departments.objects.all()
+        departaments_serializer = DepartmentSerializer(departaments, many=True)
+        return JsonResponse(departaments_serializer.data, safe=False)
+    elif request.method == 'POST':
+        department_data = JSONParser().parse(request)
+        department_serializer = DepartmentSerializer(data=department_data)
+        if department_serializer.is_valid():
+            department_serializer.save()
+            return JsonResponse('Create Good', safe=False)
+        return JsonResponse('Failed', safe=False)
+    elif request.method == 'PUT':
+        department_data = JSONParser().parse(request)
+        department = Departments.objects.get(DepartmentsId=department_data['DepartmentsId'])
+        departments_serializers = DepartmentSerializer(department, data=department_data)
+        if departments_serializers.is_valid():
+            departments_serializers.save()
+            return JsonResponse('Update Good', safe=False)
+        return JsonResponse('Failed', safe=False)
+    elif request.method == 'DELETE':
+        department = Departments.objects.get(DepartmentsId=id)
+        department.delete()
+        return JsonResponse('Delete Good', safe=False)
+
+@csrf_exempt
+def employeesApi(request, id=0):
+    if request.method == 'GET':
+        employees = Employees.objects.all()
+        employees_serializer = EmployessSerializer(employees, many=True)
+        return JsonResponse(employees_serializer.data, safe=False)
+    elif request.method == 'POST':
+
+        employees_data = JSONParser().parse(request)
+        employees_serializer = EmployessSerializer(data=employees_data)
+        if employees_serializer.is_valid():
+            employees_serializer.save()
+            return JsonResponse('Create Good', safe=False)
+        return JsonResponse('Failed', safe=False)
+    elif request.method == 'PUT':
+        employees_data = JSONParser().parse(request)
+        employees = Employees.objects.get(EmployeesId=employees_data['EmployeesId'])
+        employees_serializers = EmployessSerializer(employees, data=employees_data)
+        if employees_serializers.is_valid():
+            employees_serializers.save()
+            return JsonResponse('Update Good', safe=False)
+        return JsonResponse('Failed', safe=False)
+    elif request.method == 'DELETE':
+        employees = Employees.objects.get(EmployeesId=id)
+        employees.delete()
+        return JsonResponse('Delete Good', safe=False)
+
+
+@csrf_exempt
+def saveImgApi(request):
+    file = request.FILES['file']
+    file_name = default_storage.save(file.name,file)
+    return JsonResponse(file_name, safe=False)
+
+def index(request):
+    return render(request, 'mongo/index.html', {'var': 'var'})
